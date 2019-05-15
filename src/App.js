fe0e59de7a28b04f04aa8e7c565861df61cf070d
Nilomiranda <nilomiranda3@gmail.com>
2019-05-14 22:20:45 -0300

    Initial setyp for the project --eslint --prettier

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ReactMapGL from 'react-map-gl';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        latitude: 37.75,
+        longitude: -122.43,
+        zoom: 2,
+      },
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.resizeWindow);
+    this.resizeWindow();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.resizeWindow);
+  }
+
+  resizeWindow() {
+    const { viewport } = this.state;
+    this.setState({
+      viewport: {
+        ...viewport,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      },
+    });
+  }
+
+  render() {
+    const { viewport } = this.state;
+    return (
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken="pk.eyJ1IjoiZGFubWlyYW5kYSIsImEiOiJjanZuNHkzMTQxazRyNDhxbGluZ2E4YXpvIn0.8OPQ_CoSC3_dpHM71V0-Lg"
+        onViewportChange={newViewport => this.setState({ viewport: newViewport })}
+        mapStyle="mapbox://styles/mapbox/basic-v9"
+      >
+        <h1>Hello</h1>
+      </ReactMapGL>
+    );
+  }
 }
-
-export default App;
