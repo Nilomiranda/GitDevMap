d15434e2f095786daed78c4a9c4a1ab0e9dad9ac
Nilomiranda <nilomiranda3@gmail.com>
2019-05-21 22:31:34 -0300

    Fix to modal window to add user

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 // eslint-disable-next-line import/prefer-default-export
 export const Card = styled.div`
+  position: absolute;
   width: inherit;
   height: inherit;
   display: flex;
